3975e80d1a78ff2232b3cb7e2dc66147fe31bb5f
shaurya-blip <shaurya.p.singh21@gmail.com>
2022-07-31 17:00:04 +0530

    Import 'Books.vue' and 'PageNotFound.vue'

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,8 +3,10 @@ import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 import About from "../views/About.vue";
 import Posts from "../views/Posts.vue";
-import Post from "../views/Post.vue";
+// import Post from "../views/Post.vue";
 import Contact from "../views/Contact.vue";
+import PageNotFound from "../views/PageNotFound.vue";
+import Books from "../views/Books.vue";
 
 Vue.use(VueRouter);
 
@@ -21,25 +23,40 @@ const routes = [
   },
   {
     path: "/blogs",
-    name: "Post",
+    name: "Blogs",
     component: Posts,
   },
   {
     path: "/contact",
-    name: "contact",
+    name: "Contact",
     component: Contact,
   },
+  // {
+  //   path: "/blog/:slug",
+  //   name: "postd",
+  //   component: Post,
+  //   props: true,
+  // },
   {
-    path: "/blog/:slug",
-    name: "postd",
-    component: Post,
-    props: true,
+    path:"*",
+    name:"404",
+    component: PageNotFound
   },
+  {
+    path:"/books/",
+    name:"Books",
+    component:Books
+  }
 ];
 
 const router = new VueRouter({
   mode: "history",
   routes
+});
+
+router.beforeEach((to, from, next) => {
+  document.title = to.name + " | SBlip.dev";
+  next();
 });
 
 export default router;
