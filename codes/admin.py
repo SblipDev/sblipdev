429c56b8a5bd08d48751ecdea2a78bed6592d318
shaurya-blip <shaurya.p.singh21@gmail.com>
2022-07-31 16:51:50 +0530

    Added Book Model to admin.py

--- codes/admin.py
+++ codes/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Post, Reply
+from .models import Post, Reply, Book
 # Register your models here.
 
 admin.site.register(Post)
 admin.site.register(Reply)
+admin.site.register(Book)
